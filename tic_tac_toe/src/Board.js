649d2d4b9859cf126002f5f63c9e48c1b8686c44
pawel02 <pawel.bes13@gmail.com>
2021-08-08 12:15:39 +0100

    added filling the board with x and o

--- tic_tac_toe/src/Board.js
+++ tic_tac_toe/src/Board.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import Square from './Square';
 
+function Square(props)
+{
+    return (
+        <button className = "square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
 
 class Board extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    handleClick(i)
+    {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
     renderSquare(i)
     {
-        return <Square value={i} />
+        return <Square 
+        value={this.state.squares[i]}
+        onClick={() => { this.handleClick(i); }} />
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     
         return (
           <div class = "board">

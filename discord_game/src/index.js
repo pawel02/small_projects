12bcc4492dfae9487560bf360ee8373adc0c3ac7
pawel02 <pawel.bes13@gmail.com>
2021-08-13 13:05:33 +0100

    added comments

--- discord_game/src/index.js
+++ discord_game/src/index.js
@@ -1,16 +1,27 @@
+// There is a lot of reading files
 const fs = require('fs');
+
+// needed imports for registering commands
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+
+// Get the client id and the token
 const CLIENT_ID = fs.readFileSync('./clientID.txt', { encoding:'utf8', flag:'r' });
 const token = fs.readFileSync('./token.txt', { encoding:'utf8', flag:'r' });
 
+// common for discord API
 const { Client, Intents, Collection } = require('discord.js');
 
+// start a new client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// will hold all client commands imported from commadns dir
 client.commands = new Collection();
+
+// temporarily stores the commands imported from commands dir
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// imports all the commands
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
@@ -20,9 +31,11 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(token);
 
 client.once('ready', () => {
-    console.log(CLIENT_ID);
+
+    // when the bot starts it will register all the commands for the servers that it is in
+    // For production this should be using applicationCommands with the clientID but that will only
+    // update the commands every hour instead of instantly which is needed for development
     client.guilds.cache.each(guild => {
-        // when the bot starts it will register all the commands for the servers that it is in
         (async () => {
             try {
                 console.log('Started refreshing application (/) commands.');
@@ -44,6 +57,7 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
+    // if we have the command then execute it
     if (!client.commands.has(interaction.commandName)) return;
 
     try {
@@ -55,11 +69,11 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-
+// start the client
 client.login(token);
 
+// for gracefully shutting down the client using ctlr + c
 process.on('SIGINT', function() {
     console.log('Gracefully shutting down application');
-    // some other closing procedures go here
     process.exit(1);
 });
